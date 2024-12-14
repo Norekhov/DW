c940f7156e98c23aa9703c57b0a30b8f84721552
Exever <4ex4ever4ex@gmail.com>
2024-12-15 00:43:35 +0500

    DTO makeup

--- src/main/java/ru/skypro/homework/mapper/UserMapper.java
+++ src/main/java/ru/skypro/homework/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import ru.skypro.homework.config.ApplicationConfig;
 import ru.skypro.homework.dto.RegisterDto;
 import ru.skypro.homework.dto.Role;
 import ru.skypro.homework.dto.UserApiDto;
+import ru.skypro.homework.dto.UserAuthenticationDto;
 import ru.skypro.homework.model.User;
 
 public class UserMapper {
@@ -22,7 +23,15 @@ public class UserMapper {
         return userApiDto;
     }
 
+    public static UserDetails toUserDetails(User user) {
+        return new UserAuthenticationDto(user.getUsername(), user.getPassword(), user.getRole(), user.isEnabled());
+    }
+    public static UserDetails toUserDetails(RegisterDto user) {
+        return new UserAuthenticationDto(user.getUsername(), user.getPassword(), user.getRole(), 1);
+    }
+
     public static User toUser(RegisterDto registerDto) {
+
         return new User(null, registerDto.getUsername(), registerDto.getFirstName(), registerDto.getLastName(), registerDto.getPhone(), registerDto.getRole(), registerDto.getPassword(), 1, null);
     }
 
